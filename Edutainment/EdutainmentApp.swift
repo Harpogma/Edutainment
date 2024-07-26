d2d0783ae972d3c46839b5198f727244ee085dbf
Harpogma <gd.temp@pm.me>
2024-07-26 10:16:59 +0200

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Gregory Daguerre on 26.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
